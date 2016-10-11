4d8164c19a48e4105a0f85ed037a6fb1e12a54fa
annanee <annaflavia@fluture.com.br>
2016-10-11 07:52:36 -0300

    Entries dashboard, monetize gem setup

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,30 @@
 class EntriesController < ApplicationController
 
+  respond_to :html, :xhr
+
   def index
     @entries = current_user.entries
+  end
+
+  def create
+    @entry = current_user.entries.new(entries_params)
+    @entry.save
+    respond_with @entry
+    # respond_to do |format|
+    #   if @entry.save
+    #     format.js { render json: @entry }
+    #   else
+    #     format.js { render json: @entry.errors.messages }
+    # end
+  end
+
+  private
+
+  def entries_params
+    params.require(:entry).permit(:released_at,
+                                  :description,
+                                  :kind,
+                                  :amount_cents)
   end
 
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,8 +1,25 @@
 class Entry < ApplicationRecord
+
   belongs_to :user
+
   validates :amount_cents, presence: true, numericality:  { greater_than: 0 }
   validates :released_at, presence: true
   validates :description, presence: true
   validates :kind, presence: true
   validates :user, presence: true
+
+  monetize :amount_cents
+
+  scope :incomes, -> { where(kind: "income") }
+  scope :expenses, -> { where(kind: "expenses") }
+
 end
+
+
+# total_income = current_user.entries.incomes.sum(:amount)
+# total_expenses = current_user.entries.expenses.sum(:amount)
+
+
+# balance = total_income - total_expenses
+
+# balance.format
